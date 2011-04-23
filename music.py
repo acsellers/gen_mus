04854e4508626679736d9e541f587c49cda7ae89
Andrew Sellers <andrew@andrewcsellers.com>
2011-04-23 07:46:08 -0500

    Working, though there's still some background noise to be removed

--- music.py
+++ music.py
@@ -3,12 +3,10 @@
 import random,sys
 from struct import *
 
-rate = 12000
-
+rate = 6000
 
 def wl():
     return (rate/160.0)*(0.87055**(int(random.random()*10)))
-    
 if __name__ == '__main__':
     
     #number chosen by fair dice roll, guaranteed to be random
@@ -16,52 +14,55 @@ if __name__ == '__main__':
     
     wla = wl()
     
-    while True:
+    playing = True
+    
+    while playing:
         wlb=wla
         wla=wl()
         
         if wla==wlb:
-            wla *=2
+            wla = wla * 2
             
-        d=random.randint(5,15)*(rate/4)
+        d=int(random.random()*10+5)*rate/4
         
-        a=0
-        b=0
-        c=128
+        a=0.0
+        b=0.0
+        c=128.0
         ca=40/wla
         cb=20/wlb
-        de=rate/10
+        de=rate/40
         di=0
+        
         for i in range(0,d):
-            a+=1
-            b+=1
+            a+=1.0
+            b+=1.0
             di+=1
-            c+=ca
-            c+=cb
-            
+            c=(c + ca + cb)
+
             if (a>wla):
-                
-                a=0
-                ca *= -1
+                a=0.0
+                ca=-ca
                 
             if (b>wlb):
-                b=0
-                cb *= -1
+                b=0.0
+                cb=-cb
             
             if (di>de):
                 di=0
-                ca *= 0.9
-                cb *= 0.9
+                ca = ca * 0.9
+                cb = cb * 0.9
+
+
+            
+            sys.stdout.write(pack('B',int(c)))
+        
+        c=int(c)
+        while c!=128:
+            
+            if c>128:
+                c-=1
+            else:
+                c+=1
             
             sys.stdout.write(pack('B',c))
-            
-            c=int(c)
-            
-            while c!=128:
-                
-                if c>128:
-                    c-=1
-                else:
-                    c+=1
-                
-                sys.stdout.write(pack('B',c))
+
